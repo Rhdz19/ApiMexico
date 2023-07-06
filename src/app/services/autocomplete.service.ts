816dbccce6a4caf1f572621db4c36c9b8a30ab83
Rhdz19 <L19530321@cancun.tecnm.mx>
2023-07-05 22:23:24 -0500

    Correccion de errores

--- src/app/services/autocomplete.service.ts
+++ src/app/services/autocomplete.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormControl, UntypedFormGroup, Validators } from "@angular/forms";
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
+  
 })
 export class AutocompleteService {
   timer: any;
